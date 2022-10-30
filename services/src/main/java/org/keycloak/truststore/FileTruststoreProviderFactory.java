cb67104f54e886372326f956eaf9a4794d5742d4
Prasad Subramanian <prasad.subramanian@dell.com>
2022-10-30 12:43:15 -0600

    changes to file truststore to load truststore more often vs. only at initialization time

--- services/src/main/java/org/keycloak/truststore/FileTruststoreProviderFactory.java
+++ services/src/main/java/org/keycloak/truststore/FileTruststoreProviderFactory.java
@@ -24,32 +24,14 @@ import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PublicKey;
-import java.security.SignatureException;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import javax.security.auth.x500.X500Principal;
 
 /**
  * @author <a href="mailto:mstrukel@redhat.com">Marko Strukelj</a>
  */
-public class FileTruststoreProviderFactory implements TruststoreProviderFactory {
+public class
+FileTruststoreProviderFactory implements TruststoreProviderFactory {
 
     private static final Logger log = Logger.getLogger(FileTruststoreProviderFactory.class);
 
@@ -62,61 +44,10 @@ public class FileTruststoreProviderFactory implements TruststoreProviderFactory 
 
     @Override
     public void init(Config.Scope config) {
-
-        String storepath = config.get("file");
-        String pass = config.get("password");
-        String policy = config.get("hostname-verification-policy");
-
-        // if "truststore" . "file" is not configured then it is disabled
-        if (storepath == null && pass == null && policy == null) {
-            return;
-        }
-
-        HostnameVerificationPolicy verificationPolicy = null;
-        KeyStore truststore = null;
-
-        if (storepath == null) {
-            throw new RuntimeException("Attribute 'file' missing in 'truststore':'file' configuration");
-        }
-        if (pass == null) {
-            throw new RuntimeException("Attribute 'password' missing in 'truststore':'file' configuration");
-        }
-
-        try {
-            truststore = loadStore(storepath, pass == null ? null :pass.toCharArray());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize TruststoreProviderFactory: " + new File(storepath).getAbsolutePath(), e);
-        }
-        if (policy == null) {
-            verificationPolicy = HostnameVerificationPolicy.WILDCARD;
-        } else {
-            try {
-                verificationPolicy = HostnameVerificationPolicy.valueOf(policy);
-            } catch (Exception e) {
-                throw new RuntimeException("Invalid value for 'hostname-verification-policy': " + policy + " (must be one of: ANY, WILDCARD, STRICT)");
-            }
-        }
-
-        TruststoreCertificatesLoader certsLoader = new TruststoreCertificatesLoader(truststore);
-        provider = new FileTruststoreProvider(truststore, verificationPolicy, Collections.unmodifiableMap(certsLoader.trustedRootCerts)
-                , Collections.unmodifiableMap(certsLoader.intermediateCerts));
-        TruststoreProviderSingleton.set(provider);
-        log.debug("File truststore provider initialized: " + new File(storepath).getAbsolutePath());
+        FileTruststoreProvider fileTruststoreProvider = new FileTruststoreProvider(config);
     }
 
-    private KeyStore loadStore(String path, char[] password) throws Exception {
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        InputStream is = new FileInputStream(path);
-        try {
-            ks.load(is, password);
-            return ks;
-        } finally {
-            try {
-                is.close();
-            } catch (IOException ignored) {
-            }
-        }
-    }
+
 
     @Override
     public void postInit(KeycloakSessionFactory factory) {
@@ -154,79 +85,5 @@ public class FileTruststoreProviderFactory implements TruststoreProviderFactory 
                 .build();
     }
 
-    private static class TruststoreCertificatesLoader {
 
-        private Map<X500Principal, X509Certificate> trustedRootCerts = new HashMap<>();
-        private Map<X500Principal, X509Certificate> intermediateCerts = new HashMap<>();
-
-
-        public TruststoreCertificatesLoader(KeyStore truststore) {
-            readTruststore(truststore);
-        }
-
-        /**
-         * Get all certificates from Keycloak Truststore, and classify them in two lists : root CAs and intermediates CAs
-         */
-        private void readTruststore(KeyStore truststore) {
-
-            //Reading truststore aliases & certificates
-            Enumeration enumeration;
-
-            try {
-
-                enumeration = truststore.aliases();
-                log.trace("Checking " + truststore.size() + " entries from the truststore.");
-                while(enumeration.hasMoreElements()) {
-
-                    String alias = (String)enumeration.nextElement();
-                    Certificate certificate = truststore.getCertificate(alias);
-
-                    if (certificate instanceof X509Certificate) {
-                        X509Certificate cax509cert = (X509Certificate) certificate;
-                        if (isSelfSigned(cax509cert)) {
-                            X500Principal principal = cax509cert.getSubjectX500Principal();
-                            trustedRootCerts.put(principal, cax509cert);
-                            log.debug("Trusted root CA found in trustore : alias : "+alias + " | Subject DN : " + principal);
-                        } else {
-                            X500Principal principal = cax509cert.getSubjectX500Principal();
-                            intermediateCerts.put(principal, cax509cert);
-                            log.debug("Intermediate CA found in trustore : alias : "+alias + " | Subject DN : " + principal);
-                        }
-                    } else
-                        log.info("Skipping certificate with alias ["+ alias + "] from truststore, because it's not an X509Certificate");
-
-                }
-            } catch (KeyStoreException e) {
-                log.error("Error while reading Keycloak truststore "+e.getMessage(),e);
-            } catch (CertificateException e) {
-                log.error("Error while reading Keycloak truststore "+e.getMessage(),e);
-            } catch (NoSuchAlgorithmException e) {
-                log.error("Error while reading Keycloak truststore "+e.getMessage(),e);
-            } catch (NoSuchProviderException e) {
-                log.error("Error while reading Keycloak truststore "+e.getMessage(),e);
-            }
-        }
-
-        /**
-         * Checks whether given X.509 certificate is self-signed.
-         */
-        private boolean isSelfSigned(X509Certificate cert)
-                throws CertificateException, NoSuchAlgorithmException,
-                NoSuchProviderException {
-            try {
-                // Try to verify certificate signature with its own public key
-                PublicKey key = cert.getPublicKey();
-                cert.verify(key);
-                log.trace("certificate " + cert.getSubjectDN() + " detected as root CA");
-                return true;
-            } catch (SignatureException sigEx) {
-                // Invalid signature --> not self-signed
-                log.trace("certificate " + cert.getSubjectDN() + " detected as intermediate CA");
-            } catch (InvalidKeyException keyEx) {
-                // Invalid key --> not self-signed
-                log.trace("certificate " + cert.getSubjectDN() + " detected as intermediate CA");
-            }
-            return false;
-        }
-    }
 }
